fd76a1a368ab0d1b90bb8ffce56718e92bae930e
Christopher Walle <cjwalle97@gmail.com>
2018-05-31 15:06:15 -0500

    Update #2 5/31/2018
    Update

--- Bubble_sorting.py
+++ Bubble_sorting.py
@@ -1,5 +1,23 @@
-def Bubble(list):
-    '''sorts objects in a list via the Bubble Sort method'''
-    
-    def swapplaces(pos1, pos2, num1, num2):
-        
+class Result(object):
+    def __init__(self, value, lock)
+    self.value = value
+    self.lock = False
+
+def Bubble(sortable):
+    slist = sortable
+    ordered = False
+    for i in slist:
+        ordered = isgreater(slist[i].value, slist[i + 1].value)
+        if ordered == False
+
+def swapplaces(pos1, pos2, num1, num2, sortable):
+    slist = sortable
+    slist[pos1] = num2
+    slist[pos2] = num1
+    return slist
+
+def isgreater(num1, num2):
+    if num1 > num2:
+        return True
+    else:
+        return False
