b948a0f2877afbc6bad96ae060cb7e8c1871053a
BlaneTorregrossa <blanet97@cox.net>
2018-06-08 06:28:59 -0500

    executable created

--- Path and Pass Scoring/main.py
+++ Path and Pass Scoring/main.py
@@ -1,4 +1,8 @@
-# Change how modules are imported
+# Change modules imported
+
+from tkinter import *
+from tkMessageBox import *
+from os.path import join
 
 #   Algorithm 1
 import Blane_pathfinding_test
@@ -16,7 +20,6 @@ from Christopher_AStar_test import christopheralt_graph
 from Christopher_AStar_test import christopheralt_astar
 
 #   Does not return anything but adds to lists
-#   Needs smaller edits ***
 def PassFail(passlist, giventest, actualpath, givengraph, currentalgo):
     passcheck = False
     copygraph = list(givengraph)
@@ -53,57 +56,64 @@ def PassFail(passlist, giventest, actualpath, givengraph, currentalgo):
 
 
 
-#   To get the difference in position between each node of two paths 
-#   and give a score based off of how close they are    ***
-def getpathscore(algopath, expectedpath):
-    missingnodes = 0
-    extranodes = 0
-    scorelist = []
-    finalpathscore = 100
-    scoresubtract = 0
-    scorestandard = 100
-    totalscores = []
+# #   To get the difference in position between each node of two paths 
+# #   and give a score based off of how close they are    ***
+# def getpathscore(algopath, expectedpath):
+#     missingnodes = 0
+#     extranodes = 0
+#     scorelist = []
+#     finalpathscore = 100
+#     scoresubtract = 0
+#     scorestandard = 100
+#     totalscores = []
 
-    if len(algopath) > len(expectedpath):
-        extranodes = (len(algopath) - len(expectedpath))
-    elif len(algopath) < len(expectedpath)):
-        missingnodes = (len(algopath) - len(expectedpath))
+#     if len(algopath) > len(expectedpath):
+#         extranodes = (len(algopath) - len(expectedpath))
+#     elif len(algopath) < len(expectedpath)):
+#         missingnodes = (len(algopath) - len(expectedpath))
     
-    testpathlength = len(algopath) - 1
-    scoresubtract = finalpathscore / testpathlength
+#     testpathlength = len(algopath) - 1
+#     scoresubtract = finalpathscore / testpathlength
 
-    #   Do this in reverse order    ***
-    while testpathlength > 0:
-        ydifference = abs(algopath[testpathlength][1] - expectedpath[testpathlength][1])
-        xdifference = abs(algopath[testpathlength][0] - expectedpath[testpathlength][0])
+#     #   Do this in reverse order    ***
+#     while testpathlength > 0:
+#         ydifference = abs(algopath[testpathlength][1] - expectedpath[testpathlength][1])
+#         xdifference = abs(algopath[testpathlength][0] - expectedpath[testpathlength][0])
 
-        score = xdifference + ydifference
-        scorelist.append(score)
-        testpathlength -= 1
+#         score = xdifference + ydifference
+#         scorelist.append(score)
+#         testpathlength -= 1
     
 
-    # Scoring of each individual node and how close they are to expected position
-    for i in range(0, len(scorelist)):
-        if scorelist[i] < 0:
-            totalscores.append(scorestandard)
-        if scorelist[i] > 0:
-            totalscores.append(scorestandard - scorelist[i])
-            if totalscores[len(totalscores) - 1] < 0:
-                totalscores[len(totalscores) - 1] = 0
+#     # Scoring of each individual node and how close they are to expected position
+#     for i in range(0, len(scorelist)):
+#         if scorelist[i] < 0:
+#             totalscores.append(scorestandard)
+#         if scorelist[i] > 0:
+#             totalscores.append(scorestandard - scorelist[i])
+#             if totalscores[len(totalscores) - 1] < 0:
+#                 totalscores[len(totalscores) - 1] = 0
     
 #   Takes given results from the passfail and pathscore tests and creates a final grade
 #   based off of the previous test results  ***
-def finalgrade(pfresults, pathresults):
+# def finalgrade(pfresults, pathresults):
+def finalgrade(pfresults):
     pflength = len(pfresults)
+    removalnum = 0
+    
     for i in pfresults:
-        passscore = pflength / len(pathresults)
+        if pfresults[i - 1] == False:
+            removalnum += 1
+    passscore = pflength / removalnum
 
     # any additional math for pathresults
 
-    passscore = passscore / 2
-    pathresults = pathresults / 2
+    # passscore = passscore / 2
+    # pathresults = pathresults / 2
     
-    return pathresults + passscore 
+    # return pathresults + passscore 
+
+    return passscore
 
 # standard insertion algorithm
 def insertiontest(unarrangedlist):
@@ -128,40 +138,40 @@ def main():
 
     testlist = [test1, test2, test3]
     testsmax = len(testlist) - 1
+    resultFile = "path and pass score.txt"
     remainingtests = 0 
     currentresults = []
     givenpaths = []
     pathresults = []
     arrangement = []
-    usedgraphs = [christopher_graph, blane_graph, christopheralt_graph]
-    algolist = [christopher_astar, blane_astar, christopheralt_astar]
+    usedgraphs = [christopher_graph]
+    algolist = [christopher_astar]
 
     # to go trough each test case in the list and run the test
     # Pass-Fail then path accuracy
-    for i in range(0, len(algolist) - 1):
+    for i in range(0, len(algolist)):
         while testsmax >= remainingtests:
-            print(i)
-            print("Pre Pass Fail")
             PassFail(currentresults, testlist[remainingtests],
                 givenpaths, usedgraphs[i], algolist[i])
-            print("Post Pass Fail")
             # getpathscore()
             remainingtests += 1
-            # arrangement.append(finalgrade(currentresults, pathresults))
+            arrangement.append(finalgrade(currentresults))
 
 
-    #   Should name algorithm in list with score when written to file ***
-    #   Algorithm names here are not accurate
     #   standard insertion
     insertiontest(arrangement)
-    for i in range(len(arrangement)):
-        with open('sortingresults.txt', 'a') as r:
+    towrite = len(arrangement)
+    print(towrite)
+    counter = 0
+    while counter <= towrite:
+        with open(resultFile, 'a') as r:
             r.write('\n' + 'Algorithm ' + repr(i) +
-            ' correctness: ' + repr(arrangement[i]))
+            ' correctness: ' + repr(arrangement[i] - 1))
+            counter += 1
+    
+    showinfo('Testing Done', "Result written out on document named: " + resultFile)
 
-
-if __name__ == '__main__':
-    main()
+main()
 
 
 
