a389ae67383564b9d95115aad8a86ba7dba4489c
Christopher Walle <cjwalle97@gmail.com>
2018-06-14 17:58:08 -0500

    Update #2 6/14/2018
    Update

--- Christopher/Christopher_Score.py
+++ Christopher/Christopher_Score.py
@@ -1,21 +1,27 @@
-from Bubble_sorting import Bubble
-
-class Pathscores(object):
+from Bubble_odescore())
+class Pathresults(object):
     def __init__(self, nodelist, totalcost):
         self.nodelist = nodelist
         self.totalcost = totalcost
         self.nodenum = countnodes(nodelist)
+        self.ns = 0 #node score
+        self.cs = 0 #cost score
 
-def sortscores(results, nodeaverage, costaverage):
+def sortscores(resultslist, nodeaverage, costaverage):
     #Sorts the list of results by the combined total of their node score and their cost score
-    
+    for r in resultslist:
+        resultslist[r].ns = nodescore(resultslist[r].nodenum, nodeaverage)
+        resultslist[r].cs = costscore(resultslist[r].totalcost, costaverage)
 
 def costscore(cost, average):
     #determines a path's cost score
+    score = average - cost
+    return score
 
-def nodescore(path, average):
+def nodescore(nodes, average):
     #determines a path's node score
-
+    score = average - nodes
+    return score
 
 def countnodes(path):
     #counts the nodes in a path
